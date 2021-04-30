3dd1e5a649e53de1f33f6d9aeda6ee0f4145b655
akorotkov <89065267799@mail.ru>
2021-04-30 17:52:05 +0300

    init commit

---
+++ urlrewrite.php
@@ -0,0 +1,27 @@
+<?php
+$arUrlRewrite=array (
+  3 => 
+  array (
+    'CONDITION' => '#^/articles/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/articles/index.php',
+    'SORT' => 100,
+  ),
+  1 => 
+  array (
+    'CONDITION' => '#^/catalog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/index.php',
+    'SORT' => 100,
+  ),
+  2 => 
+  array (
+    'CONDITION' => '#^/catalog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/catalog/index.php',
+    'SORT' => 100,
+  ),
+);
